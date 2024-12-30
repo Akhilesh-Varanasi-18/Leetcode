26db3c050a5965d0bc4d97177df9ae39418cdc59
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2024-12-30 16:06:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -0,0 +1,16 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int beg=1;
+        int end=n;
+        while(beg<end){
+            int mid=(beg+end)/2;
+            if(isBadVersion(mid)) end=mid;
+            else beg=mid+1;
+        }
+        return beg;  
+    }
+};
