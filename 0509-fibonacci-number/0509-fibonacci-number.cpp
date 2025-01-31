82deacb1b4aebdf00bdcd251b6ac50ee02a96bf4
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2025-01-31 23:00:34 +0530

    Time: 3 ms (67.88%), Space: 8.1 MB (17.22%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
-int fibvalue(int n){
+int fibvalue(int n,vector<int> &data){
     if(n<=1)return n;
-    return(fibvalue(n-1)+fibvalue(n-2));
+    if(data[n]!=-1)return data[n];
+    int left = fibvalue(n-1,data);
+    int right=fibvalue(n-2,data);
+    data[n]=left+right;
+    return left+right;
+
 }
     int fib(int n) {
-        return fibvalue(n);
+        vector<int>data(n+1,-1);
+        return fibvalue(n,data);
     }
 };
