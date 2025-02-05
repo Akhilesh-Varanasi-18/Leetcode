4b7de2f91befa25d205970e9024ba14ef29b88d6
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2025-02-05 10:55:15 +0530

    Time: 0 ms (100%), Space: 8.1 MB (97.48%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int c=0;
+        for(int i=0;i<s1.size();i++){
+            if(s1[i] != s2[i])c++;
+        }
+        sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
+        if(s1 != s2)return false;
+        if(c>2)return false;
+        else return true;
+
+    }
+};
