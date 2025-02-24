7de477f0a4a57d15cb757f87ae9531e2fc8c00b6
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2025-02-24 22:04:00 +0530

    Time: 0 ms (100%), Space: 12.3 MB (39.6%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int>res;
-        int cnt=count(nums1.begin(),nums1.end(),0);
-        int c=cnt-n;
-        for(int i=0;i<m;i++){
-            if(nums1[i]!=0)res.push_back(nums1[i]);
+        for(int j=0,i=m;j<n;j++){
+            nums1[i]=nums2[j];
+            i++;
         }
-        for(int i=0 ; i<n ; i++){
-            res.push_back(nums2[i]);
-        }
-        for(int i=0;i<c;i++){
-            res.push_back(0);
-        }
-        sort(res.begin(),res.end());
-        nums1=res;
-        // for(int i=0;i<res.size();i++){
-        //     cout<<res[i]<<" ";
-        // }
-        // cout<<endl;
+        sort(nums1.begin(),nums1.end());
     }
 };
