db744ba259ea3e2c9d5c5ae18068df61db0854c3
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2025-02-24 21:52:49 +0530

    Time: 0 ms (100%), Space: 12.4 MB (9.45%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,11 +2,16 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         vector<int>res;
+        int cnt=count(nums1.begin(),nums1.end(),0);
+        int c=cnt-n;
         for(int i=0;i<m;i++){
             if(nums1[i]!=0)res.push_back(nums1[i]);
         }
         for(int i=0 ; i<n ; i++){
             res.push_back(nums2[i]);
+        }
+        for(int i=0;i<c;i++){
+            res.push_back(0);
         }
         sort(res.begin(),res.end());
         nums1=res;
