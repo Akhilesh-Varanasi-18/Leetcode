21a85e1bd04e1c6958f140008e7aec84f132f0bb
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2025-02-27 17:08:38 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.71%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-int s=0;
     bool isHappy(int n) {
-        if(s<10 && s>0){
-            if(s==1 || s==7)return true;
-            return false;
-        }
-        s=0;
+       if(n==1 or n==7) return true;
+       else if(n<10)return false;
+       else{
+        int s=0;
         while(n>0){
-            int l=n%10;
-            s+=l*l;
-            n=n/10;
+            int res=n%10;
+            s+=res*res;
+            n/=10;
         }
         return isHappy(s);
+       } 
     }
 };
