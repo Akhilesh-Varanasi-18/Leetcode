125946e27eec8e5c300d508576de6125b40939f5
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2025-03-22 18:05:20 +0530

    Time: 7 ms (7.78%), Space: 10.7 MB (9.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,10 +4,12 @@ public:
         string res;
         for(int i=0;i<s.size();i++){
             if(isupper(s[i])) s[i]=tolower(s[i]);
-            if((s[i]>='a' and s[i]<='z') or (s[i]>=0 and s[i]<=9)) res+=s[i];
+            if((s[i]>='a' and s[i]<='z') or (s[i]>='0' and s[i]<='9')) res+=s[i];
         }
         string ans=res;
+        cout<<ans<<endl;
         reverse(res.begin(),res.end());
+        cout<<res;
         if(res==ans)return true;
         else return false;
     }
