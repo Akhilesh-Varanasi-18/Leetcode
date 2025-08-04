95e1400dda6713d1c7eac4eaef38c8d6498706be
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2025-08-04 10:47:07 +0530

    Time: 0 ms (100%), Space: 11 MB (39.99%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -6,22 +6,23 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void po(TreeNode* n, vector<int> &res) {
-        if (n == NULL)
-            return;
-        res.push_back(n->val);
-        po(n->left, res);
-        po(n->right, res);
-    }
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> res;
-        po(root, res);
-        return res;
+       vector<int>po;
+       if(root == NULL)return po;
+       stack<TreeNode*>s;
+       s.push(root);
+       while(!s.empty()){
+        TreeNode* n=s.top();
+        s.pop();
+        po.push_back(n->val);
+        if(n->right != NULL)s.push(n->right);
+        if(n->left != NULL)s.push(n->left);
+       } 
+       return po;
     }
 };
