ac0b4da2d93d2005536ed674ef7dc0377e19491d
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2025-08-07 12:04:42 +0530

    Time: 0 ms (100%), Space: 12.9 MB (47.89%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,20 +11,11 @@
  */
 class Solution {
 public:
-    void po(TreeNode* n, vector<int>& res){
-        if(n == NULL){
-            res.push_back(INT_MAX);
-            return;
-        }
-        else res.push_back(n->val);
-        po(n->left,res);
-        po(n->right,res);
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<int>r1,r2;
-        po(p,r1);
-        po(q,r2);
-        if(r1==r2)return true;
-        else return false;
+        if(p==NULL && q==NULL)return true;
+        if((p==NULL && q!= NULL)||(p!=NULL && q==NULL))return false;
+        if(p->val != q->val)return false;
+        return isSameTree(p->left,q->left) and isSameTree(p->right,q->right);;
+        
     }
 };
