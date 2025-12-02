a38f0eb6df592794baeff7ac01fcf1411f0566f6
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2025-12-02 10:42:25 +0530

    Time: 0 ms (100%), Space: 10.1 MB (68.41%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        // Transpose
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                swap(matrix[i][j], matrix[j][i]);
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
-        // Reverse
-        for (auto& row : matrix) {
-            reverse(begin(row), end(row));
+        for(int i=0; i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
         }
+        
     }
 };
