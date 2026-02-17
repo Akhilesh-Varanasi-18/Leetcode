67a2d4bb53d4549b00d7191be917a91a112f25f0
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-02-17 15:50:29 +0530

    Time: 0 ms (100%), Space: 31 MB (15.78%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int sumOfDigits(int n){
+    int SumofDigits(int n){
         int s=0;
-        while (n>0){
-            int rem=n%10;
-            s+=rem;
+        while(n>0){
+            int lastDigit=n%10;
+            s=s+lastDigit;
             n=n/10;
         }
         return s;
     }
     int smallestIndex(vector<int>& nums) {
-        for (int i=0;i<nums.size();i++){
-            if(sumOfDigits(nums[i])==i) return i;
+        for(int i=0;i<nums.size();i++){
+            if(SumofDigits(nums[i])==i){
+                return i;
+            }
         }
+
         return -1;
+        
     }
 };
