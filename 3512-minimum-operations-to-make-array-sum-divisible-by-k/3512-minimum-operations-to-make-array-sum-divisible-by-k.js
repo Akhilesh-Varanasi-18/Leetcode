1d24d3da57bd8af3cc8019b26b0aece200831d52
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-02-18 15:54:14 +0530

    Time: 1 ms (77.28%), Space: 59.2 MB (7.77%) - LeetHub

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.js
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.js
@@ -4,14 +4,17 @@
  * @return {number}
  */
 
-const sum=(arr)=>{
+const Sum = (arr) =>{//arr-->parameter
     let s=0;
-    for(let val of arr){
-        s+=val;
+    for(let i in arr){
+        // console.log(i);
+        s=s+arr[i];
     }
     return s;
 }
 var minOperations = function(nums, k) {
-    let totalSum=sum(nums);
-    return(totalSum % k);
+    let SumofArrayElements = Sum(nums)//nums-->arguement
+    // console.log(SumofArrayElements);
+    let answer = SumofArrayElements % k;
+    return answer;
 };
