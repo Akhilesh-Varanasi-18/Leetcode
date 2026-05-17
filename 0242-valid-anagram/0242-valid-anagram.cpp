c6fc8b91bc8bd4078cc81a1bcc218951c06ad647
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-05-17 12:17:11 +0530

    Time: 8 ms (6.93%), Space: 9.7 MB (48.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,14 @@
-class Solution {
+class Solution {            
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t)return true;
-        else return false;
+        if(s.length() != t.length()){
+            return false;
+        }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if(s == t){
+            return true;  
+        }
+        return false;
     }
-};
+}; 
