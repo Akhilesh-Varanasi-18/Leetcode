440877a9719fcab83773d1debb82b21af053e3b5
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-05-25 13:49:02 +0530

    Time: 0 ms (100%), Space: 7.8 MB (48.19%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n == 2 or n==1)return true;
+        if(n%2 != 0 or n<= 0)return false;
+        return isPowerOfTwo(n/2);
+    }
+};
