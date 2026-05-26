8b10c0fd82bfa73e4a8462d8ea2599fac547ddb0
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-05-26 12:06:54 +0530

    Time: 0 ms (100%), Space: 8.9 MB (7.08%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans = 1.0;
-        long long nn=n;
-        if(nn<0)nn=-1*nn;
-        while(nn){
-            if(nn%2){
-                ans=ans*x;
-                nn=nn-1;
-            }
-            else{
-                x=x*x;
-                nn=nn/2;
-            }
-        }
-        if(n<0)return(double(1.0)/double(ans));
-        return ans;
+        if(n==0)return 1;
+        else if(n==1)return x;
+        else if(n<0)return 1/x*(pow(x,n+1));
+        else return x*pow(x,n-1);
     }
 };
