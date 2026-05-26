eb86f899ca51a9d279f641a2aac38c5e418244bc
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-05-26 09:24:27 +0530

    Time: 3 ms (29%), Space: 10.5 MB (5.34%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        int c,s_cnt =0;
+        unordered_map<char,int>mp,mp2;
+        for(auto i: word)mp[i]++;
+        string smalls = "abcdefghijklmnopqrstuvwxyz";
+        string caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for(auto i : mp){
+            if(i.first >= 'A' and i.first <= 'Z'){
+                char ch = i.first;
+                ch = tolower(ch);
+                mp2[ch]++;
+            }
+            mp2[i.first]++;
+        }  
+        for(auto i : mp2){
+            if(i.second >= 2)s_cnt++;
+        };
+        return s_cnt; 
+    }
+};
