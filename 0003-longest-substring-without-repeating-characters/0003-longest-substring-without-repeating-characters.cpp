3d7ebde10695ea280f22c281863d86080f941a2c
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-06-18 10:42:23 +0530

    Time: 681 ms (5%), Space: 247.5 MB (5.01%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,14 +5,13 @@ public:
         //how to generate all substrings simple use i and j=i+1 and traverse
         int maxyy  = INT_MIN;
         for(int i=0;i<s.size();i++){
-            string a = "";
+            // string a = "";
             unordered_map<char,int>mp;
             for(int j=i;j<s.size();j++){
                 mp[s[j]]++;
                 if(mp[s[j]]>1)break;
                 else{
-                    a+=s[j];
-                    maxyy = max(maxyy, (int)a.size());
+                    maxyy = max(maxyy, j-i+1);
                 }
             }
         }
