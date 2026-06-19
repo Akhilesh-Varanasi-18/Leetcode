73c5c1ce939986bfb2b63ba4ad4a204554ab172f
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-06-19 13:19:30 +0530

    Time: 0 ms (100%), Space: 10.9 MB (44.68%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int g = 0 , m = 0;
+        for(auto i: gain){
+            g+=i;
+            m = max(m,g);
+        }
+        return m;
+    }
+};
