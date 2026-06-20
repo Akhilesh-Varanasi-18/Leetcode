a566b53396a06deef6eb128d6cc47fb6bff719e3
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-06-20 17:59:59 +0530

    Time: 0 ms (100%), Space: 9.1 MB (84.93%) - LeetHub

--- 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.cpp
+++ 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     vector<int> numberOfLines(vector<int>& widths, string s) {
-
-        map<char, int> mp;
-
-        for (int i = 0; i < 26; i++) {
-            mp['a' + i] = widths[i];
-        }
-
-        int i = 0;
-
-        int cnt = 1, sum = 0;
-
-        while (i < s.size()) {
-
-            cout << mp[s[i]] << " ";
-            sum += mp[s[i]];
-
-            if (sum == 100 && i == s.size() - 1) {
-                break;
-
-            } else if (sum > 100) {
-                sum = mp[s[i]];
-                cnt++;
+        int line = 1;
+        int sum = 0;
+        for (int i = 0; i < s.size(); i++) {
+            sum += widths[s[i] - 'a'];
+            cout << s[i] << "=" << widths[s[i] - 'a']<<" ";
+            if (sum > 100) {
+                cout<<endl;
+                i--;
+                line++;
+                sum = 0;
             }
-            i++;
         }
-        vector<int> ans;
-
-        ans.push_back(cnt);
-        ans.push_back(sum);
-
-        return ans;
+        return {line, sum};
     }
 };
