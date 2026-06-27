efe0915c656946c8919126c8251556f3664a5ad6
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-06-27 09:06:30 +0530

    Time: 0 ms (100%), Space: 9.2 MB (33.43%) - LeetHub

--- 1078-occurrences-after-bigram/1078-occurrences-after-bigram.cpp
+++ 1078-occurrences-after-bigram/1078-occurrences-after-bigram.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     vector<string> findOcurrences(string text, string first, string second) {
-        vector<string> words;
-        vector<string> ans;
-        string word;
-        for (int i = 0; i < text.size(); i++) {
-            if (text[i] == ' ') {
-                words.push_back(word);
+        vector<string>w;
+        string word = "";
+        for (int i=0;i<text.size();i++){
+            if(text[i] == ' '){
+                w.push_back(word);
                 word = "";
-            } else {
-                word = word + text[i];
+            }
+            else{
+                word+=text[i];
             }
         }
-        words.push_back(word);
-
-        for (int i = 0; i < words.size() - 2; i++) {
-            if (words[i] == first && words[i + 1] == second) {
-                ans.push_back(words[i + 2]);
-            }
+        w.push_back(word);
+        vector<string>res;
+        for(int i=2 ; i<w.size();i++){
+            if(w[i-1] == second and w[i-2] == first)res.push_back(w[i]); 
         }
-        return ans;
+        return res;
+        
     }
 };
