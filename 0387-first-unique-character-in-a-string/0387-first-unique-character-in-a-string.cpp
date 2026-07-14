e83e1bb2dc25ddaf3fc77f51c2359fcbb99c5676
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-07-14 09:36:33 +0530

    Time: 3 ms (80.2%), Space: 14.7 MB (74.89%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        map<char,int>mp1;
-        map<char,int>mp2;
-        int ans = INT_MAX;
-        for(auto i : s){
-            mp1[i]++;
-        }
+        vector<int>freq(26,0);
         for(int i = 0 ;i<s.size(); i++){
-           mp2[s[i]]  = i;
+            freq[s[i]-'a']++;
         }
-        for(auto i : mp2)cout<<i.first<<" "<<i.second<<endl;
-        // return 0;
-        for(auto i : mp1){
-            if(i.second == 1){
-                ans = min(ans,mp2[i.first]);
+        for(int i = 0 ; i< s.size() ; i++){
+            if(freq[s[i]-'a'] == 1){
+                return i;
             }
         }
-        if(ans == INT_MAX) return -1;
-        else return ans;
-
-        
+        return -1;
     }
 };
