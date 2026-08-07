1bba6fdb1561ec062e6de7ada8c2ce91f41dd5ad
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-08-07 18:32:14 +0530

    Time: 0 ms (100%), Space: 13.7 MB (85.96%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     bool haveConflict(vector<string>& event1, vector<string>& event2) {
-        return event1[0] <= event2[1] && event2[0]  <= event1[1];
+        vector<string>ans;
+        
+        string x = event1[1];
+        string y = event2[0];
+        string a = event1[0];
+        string b = event2[1];
+
+  
+        if(x < y || a>b) return false;
+        return true;
     }
 };
