7a47c0e5ed0a4bda8466f26a4b975ea3f341e4d9
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-08-20 13:59:37 +0530

    Time: 0 ms (100%), Space: 25 MB (5.53%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i,j;
-        i=0;
-        j=1;
-        while(i < nums.size()-1){
-            if(nums[i]==0){
-                if(j<nums.size() and nums[j] != 0){
-                    int temp = 0;
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                    i++;
-                    j=i+1;
-                }
-                else if (j<nums.size()){
-                    j++;
-                    continue;
-                }
-                else break;
-            }
-            else{
-                i++;
-                j = i+1;
-            }
+        vector<int>res;
+        int z = 0;
+        for(auto i : nums){
+            if (i != 0) res.push_back(i);
+            else z++;
         }
+        for(int i = 0;i<z;i++)res.push_back(0);
+        nums = res;
     }
 };
