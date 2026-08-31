42f5ae970ec8fc4242bfe8ae289633b5b66a6707
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-09-01 00:33:11 +0530

    Time: 11 ms (50.55%), Space: 31.9 MB (9.18%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        map<int,int>mp;
-        int n=grid.size();
-        int rep=-1;
-        int k=n*n;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++){
-                if(mp[grid[i][j]]==1)rep=grid[i][j];
-                mp[grid[i][j]]++;
+        unordered_map<int, int> freq;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid.size(); ++j) {
+                freq[grid[i][j]]++;
             }
         }
-        for(int i=1;i<=k;i++){
-            if(mp[i]==0)return{rep,i};
+        int repeat = 0, miss = 0, n = grid.size() * grid.size();
+        for (int i = 1; i <= n; ++i) {
+            if (freq[i] == 2)
+                repeat = i;
+            if (freq[i] == 0)
+                miss = i;
         }
-        return{-1,-1};  
+        return {repeat, miss};
     }
 };
