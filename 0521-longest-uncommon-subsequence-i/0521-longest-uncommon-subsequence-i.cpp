039dd70a4c2e0be943516b63465d202561aa15b0
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-09-16 15:54:09 +0530

    Time: 0 ms (100%), Space: 7.9 MB (99.91%) - LeetHub

--- 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.cpp
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int findLUSlength(string a, string b) {
-        return (a == b) ? -1 : max(a.length(), b.length());
+        if(a==b)return -1;
+        else if(a.size()>b.size())return a.size();
+        else return b.size();
     }
 };
