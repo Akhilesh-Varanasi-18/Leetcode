4e2210a36d7d4b09cf128e19f6fafa1e9b0768fa
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-09-25 15:25:29 +0530

    Time: 0 ms (100%), Space: 42.1 MB (83.86%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int numTimesAllBlue(vector<int>& flips) {
         int flip_end = flips[0];
-        int result = 0;
-        
+        int res = 0;
+
         for (int i = 0; i < flips.size(); i++) {
             if (flips[i] > flip_end) {
                 flip_end = flips[i];
             }
 
-            result += (flip_end == i + 1);
+            res += (flip_end == i + 1);
         }
 
-        return result;
+        return res;
     }
 };
